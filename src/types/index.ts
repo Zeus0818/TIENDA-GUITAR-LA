78d6c08df22b5c19e147ac79ab4302becdd04f7e
Jesus Zamudio Martinez <121834845+Zeus0818@users.noreply.github.com>
2024-05-26 23:53:21 -0500

    Primer commit

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type Guitar = {
+    id: number
+    name: string
+    image: string
+    description: string
+    price: number
+}
+
+//Heredamos Guitar a CartItem
+export type CartItem = Guitar & {
+    quantity: number
+}
+
+// export type CartItem = Pick<Guitar, 'id' | 'name' | 'price' > & {
+//     quantity: number
+// }
+// export type CartItem = Omit<Guitar, 'id' | 'name' | 'price' > & {
+//     quantity: number
+// }
+
